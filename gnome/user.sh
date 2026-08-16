1e6762ff5a28b434ca9b38b23368f9ac9f4ea4dd
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-08-16 23:38:38 +0500

    Create user.sh

---
+++ gnome/user.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.shell disable-extension-version-validation true
+
+flatpak install flathub com.mattjakeman.ExtensionManager
