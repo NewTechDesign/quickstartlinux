bfbb6411fea5f28244f80c36a0c6dc65588473df
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-08-16 23:40:13 +0500

    Create add.sh

---
+++ gnome/add.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# sudo pacman -S binwalk squashfs-tools jhead mtd-utils uboot-tools ntfs-3g dosfstools exfatprogs btrfs-progs fuse2 udisks2 gvfs 
+
+# sudo pacman -S meson ninja gcc git pkgconfig glib2 libusb nss openssl pixman systemd-libs gdk-pixbuf2 cairo base-devel scrcpy
+
+# sudo pacman -S --noconfirm jdk8-openjdk jre8-openjdk jre8-openjdk-headless xorg-xrandr jdk-openjdk
+# sudo pacman -S --noconfirm android-tools usbutils devtools
+
+# sudo pacman -S podman podman-compose
+
+# pacman -S --noconfirm virtualbox virtualbox-host-modules-arch
+# groupadd vboxusers 2>/dev/null
+# modprobe vboxdrv
