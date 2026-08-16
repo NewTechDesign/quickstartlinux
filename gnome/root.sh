17a4a61c1f6a6b1cd560c091ebb4c6a8229e61e0
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-08-16 23:38:14 +0500

    Create root.sh

---
+++ gnome/root.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+localectl set-locale ru_RU.UTF-8
+
+setxkbmap -layout us,ru -option grp:alt_shift_toggle
+
+sed -i 's/^timeout .*/timeout 1/' /boot/loader/loader.conf
+
+pacman -Suy
+pacman -S --noconfirm pacman-contrib 
+pacman -S --noconfirm gnome-tweaks adw-gtk-theme
+pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation ttf-arphic-ukai ttf-arphic-uming ttf-sazanami
