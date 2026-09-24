fe2d5ddd606ea2caea48ddd85778b3b0015f8c65
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-09-24 11:21:40 +0500

    Update add.sh

--- gnome/add.sh
+++ gnome/add.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm btrfs-progs xfsprogs f2fs-tools exfatprogs udftools ntfs-3g dosfstools e2fsprogs cryptsetup
+sudo pacman -S --noconfirm btrfs-progs xfsprogs f2fs-tools exfatprogs udftools ntfs-3g ntfsprogs dosfstools e2fsprogs cryptsetup
+
+sudo pacman -S --noconfirm binwalk squashfs-tools mtd-utils uboot-tools udisks2 usbutils
 
 sudo pacman -S --noconfirm gvfs fuse2 fuse3
 
@@ -10,12 +12,8 @@ sudo pacman -S --noconfirm android-tools scrcpy
 
 sudo pacman -S --noconfirm jhead pixman
 
-sudo pacman -S --noconfirm git base-devel devtools fakeroot meson ninja pkgconfig glib2 libusb systemd-libs gdk-pixbuf2 cairo gcc
-
 sudo pacman -S --noconfirm jdk8-openjdk jre8-openjdk jre8-openjdk-headless jdk-openjdk xorg-xrandr
 
+sudo pacman -S --noconfirm git base-devel devtools fakeroot meson ninja pkgconfig glib2 libusb systemd-libs gdk-pixbuf2 cairo gcc
+
 sudo pacman -S --noconfirm docker docker-compose
-
-sudo pacman -S --noconfirm binwalk squashfs-tools mtd-utils uboot-tools udisks2 usbutils
-
-sudo pacman -S --noconfirm mesa mesa-utils vulkan-radeon libva-mesa-driver pciutils
