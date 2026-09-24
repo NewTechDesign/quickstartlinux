1be8aabb6adc3e74f5bb3ab5e98721a7f01fa2c5
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-09-24 21:44:03 +0500

    Update root.sh

--- gnome/root.sh
+++ gnome/root.sh
@@ -8,7 +8,7 @@ sed -i 's/^timeout .*/timeout 1/' /boot/loader/loader.conf # only systemdboot
 
 pacman -Suy
 pacman -S --noconfirm pacman-contrib 
-pacman -S --noconfirm gnome-tweaks adw-gtk-theme
-pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu ttf-liberation ttf-arphic-ukai ttf-arphic-uming ttf-sazanami
+pacman -S --noconfirm adw-gtk-theme gnome-tweaks gnome-sound-recorder
+pacman -S --noconfirm ttf-dejavu ttf-liberation ttf-arphic-ukai ttf-arphic-uming ttf-sazanami noto-fonts noto-fonts-emoji noto-fonts-cjk
 
 systemctl enable --now bluetooth
