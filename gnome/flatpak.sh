0b898f47a9eb6edf23d8ac7eac6dc64c0ccf74e7
NewTechDesign <149808245+NewTechDesign@users.noreply.github.com>
2026-09-24 20:28:59 +0500

    Create flatpak.sh

---
+++ gnome/flatpak.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+flatpak install --system flathub com.mattjakeman.ExtensionManager -y
+flatpak install --system org.polymc.PolyMC -y
+flatpak install --system org.chromium.Chromium -y
+flatpak install --system us.zoom.Zoom -y
